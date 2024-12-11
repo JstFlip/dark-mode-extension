d1caac49e9efd066696ee804f440d1975057baef
Filip Leity <filip.leity@gmail.com>
2024-12-11 22:05:07 +0100

    Update style

--- scripts/content.js
+++ scripts/content.js
@@ -10,11 +10,13 @@ const toggleDarkMode = async () => {
   const enabled = await fetchWs()
   
   if (enabled) {
-    document.body.style.filter = "invert(1) hue-rotate(180deg)"
+    document.body.style.filter = "invert(0.899) hue-rotate(180deg)"
     document.body.style.background = "#1a1a1a"
+    document.body.style.minHeight = "100vh"
   } else {
     document.body.style.filter = ""
     document.body.style.background = ""
+    document.body.style.minHeight = ""
   }
 }
 
